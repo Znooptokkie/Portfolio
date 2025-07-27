ae492a6ac889ee7cd1a544b50949d5185c73d06a
Znooptokkie <oomenatilla@gmail.com>
2025-07-27 22:03:01 +0200

    REBASE: JavaScript code turned into OOP

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -42,98 +42,182 @@ class Shape
     }
 }
 
-function makeSectionVector(config)
-{
-    // if (config.name == "title")
-    // {
-    //     width = 250;
-    //     height = 50
-    // }
+class VectorShapes
+    {
+        static defaultConfig = {
+        shapeFill: "green",
+        shapeStroke: "red",
+        shapeStrokeWidth: "5",
+        fontSize: "20",
+        fontFamily: "Goldman, sans-serif",
+        fontFill: "black"
+    };
 
-    const { svgSelector, width, height, name, pathData, shapeStyles, text, textStyles } = config;
+    constructor(svgSelector, svgText, name)
+    {
+        this.svgSelector = svgSelector;
+        this.svgText = svgText;
+        this.name = name;
+        this.pathData;
+        this.shapeStyles;
+        this.width;
+        this.height;
+        this.textStyles;
+    }
 
-    const vectorShape = new Shape(
-        svgSelector,
-        "path",
+    difineWhichShape(config = {})
+    {
+        const cfg = { ...VectorShapes.defaultConfig, ...config };
+
+        switch (this.name)
         {
-            d: pathData,
-            fill: shapeStyles.fill,
-            stroke: shapeStyles.stroke,
-            "stroke-width": shapeStyles.strokeWidth,
-            transform: shapeStyles.transform || null
-        },
-        width,
-        height,
-        name
-    );
-    vectorShape.draw();
+            case "title":
+                return this.titlePropertiesArticle(cfg);
 
-    const center = vectorShape.calcCenter();
+            case "subtitle":
+                return this.subtitlePropertiesArticle(cfg)
+            
+                default:
+                return null;
+        }
+    }
 
-    const vectorText = new Shape(
-        svgSelector,
-        "text",
-        {
-            x: center.x,
-            y: center.y,
-            "text-anchor": "middle",
-            "dominant-baseline": "middle",
-            "font-size": textStyles.fontSize,
-            "font-family": textStyles.fontFamily,
-            fill: textStyles.fill
-        },
-        width,
-        height,
-        `${name}-text`
-    );
-    vectorText.create();
-    vectorText.element.textContent = text;
-    vectorText.draw();
+    titlePropertiesArticle(cfg)
+    {
+        this.pathData = "M 0 0 L 250 0 L 250 50 L 0 50 Z";
+        this.shapeStyles = {
+            fill: cfg.shapeFill,
+            stroke: cfg.shapeStroke,
+            strokeWidth: cfg.shapeStrokeWidth
+        };
+        this.width = 250;
+        this.height = 50;
+        this.textStyles = {
+            fontSize: cfg.fontSize,
+            fontFamily: cfg.fontFamily,
+            fill: cfg.fontFill
+        };
+
+        return new Shape(
+            this.svgSelector,
+            "path",
+            {
+                d: this.pathData,
+                fill: cfg.shapeFill,
+                stroke: cfg.shapeStroke,
+                "stroke-width": cfg.shapeStrokeWidth
+            },
+            this.width,
+            this.height,
+            this.name
+        );
+    }
+
+    subtitlePropertiesArticle(cfg)
+    {
+        this.pathData = "M 0 50 L 50 0 L 450 0 L 500 50 L 450 100 L 50 100 Z";
+        this.shapeStyles = {
+            fill: cfg.shapeFill,
+            stroke: cfg.shapeStroke,
+            strokeWidth: cfg.shapeStrokeWidth
+        };
+        this.width = 500;
+        this.height = 100;
+        this.textStyles = {
+            fontSize: cfg.fontSize,
+            fontFamily: cfg.fontFamily,
+            fill: cfg.fontFill
+        };
+
+        return new Shape(
+            this.svgSelector,
+            "path",
+            {
+                d: this.pathData,
+                fill: cfg.shapeFill,
+                stroke: cfg.shapeStroke,
+                "stroke-width": cfg.shapeStrokeWidth
+            },
+            this.width,
+            this.height,
+            this.name
+        );
+    }
+
+    render(config = {})
+    {
+        const shape = this.difineWhichShape(config);
+        const text = this.createTextShape(shape);
+        shape.draw();
+        text.draw();
+    }
+
+    createTextShape(shape)
+    {
+        const center = shape.calcCenter();
+
+        const textShape = new Shape(
+            this.svgSelector,
+            "text",
+            {
+                x: center.x,
+                y: center.y,
+                "text-anchor": "middle",
+                "dominant-baseline": "middle",
+                "font-size": this.textStyles.fontSize,
+                "font-family": this.textStyles.fontFamily,
+                fill: this.textStyles.fill
+            },
+            this.width,
+            this.height,
+            `${this.name}-text`
+        );
+
+        textShape.create();
+        textShape.element.textContent = this.svgText;
+
+        return textShape;
+    }
+
 }
+
+// CV - Introduction Title
+const introductionTitleShape = new VectorShapes(
+    ".cv-intro-section-title svg",
+    "CV - Introduction",
+    "title",
+)
+
+// CV - Introduction Subtitle
+const introductionSubtitleShape = new VectorShapes(
+    ".cv-intro-section-subtitle svg",
+    "Who am I?",
+    "subtitle"
+)
+
+// CV - Experience Title
+const experienceTitleShape = new VectorShapes(
+    ".cv-exp-section-title svg",
+    "CV - Motivation",
+    "title"
+)
+
+// CV - Eperience Subtitle
+const experienceSubtitleShape = new VectorShapes(
+    ".cv-exp-section-subtitle svg",
+    "Why Programming?",
+    "subtitle"
+)
 
 document.addEventListener("DOMContentLoaded", () => 
 {
-    makeSectionVector({
-        svgSelector: ".cv-intro-section-title svg",
-        width: 250,
-        height: 50,
-        name: "title",
-        pathData: "M 0 0 L 250 0 L 250 50 L 0 50 Z",
-        shapeStyles: { fill: "yellow", stroke: "green", strokeWidth: "5" },
-        text: "CV - Introduction",
-        textStyles: { fontSize: "20", fontFamily: "Goldman, sans-serif", fill: "black" }
+    introductionTitleShape.render();
+    introductionSubtitleShape.render({
+        shapeFill: "lightgrey",
+        shapeStroke: "grey",
+        shapeStrokeWidth: "2"
     });
 
-    makeSectionVector({
-        svgSelector: ".cv-intro-section-subtitle svg",
-        width: 500,
-        height: 100,
-        name: "subtitle",
-        pathData: "M 0 50 L 50 0 L 450 0 L 500 50 L 450 100 L 50 100 Z",
-        shapeStyles: { fill: "cyan", stroke: "red", strokeWidth: "3" },
-        text: "Who am I?",
-        textStyles: { fontSize: "20", fontFamily: "Goldman, sans-serif", fill: "black" }
-    });
-
-    makeSectionVector({
-        svgSelector: ".cv-exp-section-title svg",
-        width: 250,
-        height: 50,
-        name: "title",
-        pathData: "M 0 0 L 250 0 L 250 50 L 0 50 Z",
-        shapeStyles: { fill: "yellow", stroke: "green", strokeWidth: "5" },
-        text: "CV - Motivation",
-        textStyles: { fontSize: "20", fontFamily: "Goldman, sans-serif", fill: "black" }
-    });
-
-    makeSectionVector({
-        svgSelector: ".cv-exp-section-subtitle svg",
-        width: 500,
-        height: 100,
-        name: "subtitle",
-        pathData: "M 0 50 L 50 0 L 450 0 L 500 50 L 450 100 L 50 100 Z",
-        shapeStyles: { fill: "cyan", stroke: "red", strokeWidth: "3" },
-        text: "Why Programming?",
-        textStyles: { fontSize: "20", fontFamily: "Goldman, sans-serif", fill: "black" }
-    });
+    experienceTitleShape.render();
+    experienceSubtitleShape.render();
 });
