52fb97f76a6c09c840b437bc221b33a8e276d9a8
Znooptokkie <oomenatilla@gmail.com>
2025-08-20 21:07:42 +0200

    .gitignore changed

---
+++ app/static/js/main.js
@@ -0,0 +1,10 @@
+import { SvgFactoryClass } from "./svg/svgFactory.js";
+import { Dropdown } from "./controls.js";
+const svgFactory = new SvgFactoryClass();
+const callDropdownClass = new Dropdown();
+document.addEventListener("DOMContentLoaded", () => {
+    svgFactory.contentForDOMContentLoaded();
+    callDropdownClass.checkForButton();
+});
+window.addEventListener("resize", () => {
+});
